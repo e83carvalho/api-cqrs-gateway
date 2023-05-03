29e58621e552fc79ebd3b706d1bd07e9efe74d17
ergcarvalho <ergcarvalho@rd.com.br>
2023-05-02 21:27:42 -0300

    first commit

---
+++ src/main/java/br/com/egc/api/cqrsgateway/apicqrsgateway/ApiCqrsGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.egc.api.cqrsgateway.apicqrsgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCqrsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCqrsGatewayApplication.class, args);
+	}
+
+}
